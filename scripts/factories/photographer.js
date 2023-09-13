57f21af50f82888f57a7fe1cd1c758d2397ea033
Adélie Chauvel <adelie.chauvel@yahoo.fr>
2023-09-13 21:20:25 +0200

    importation userCard data

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,33 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
+
+        const photographerName = document.createElement('h2');
+        photographerName.innerText = name;
+
+        const localisation = document.createElement("p");
+        localisation.innerText = `${city}, ${country}`;
+
+        const pTagline = document.createElement("p");
+        pTagline.innerText = tagline;
+
+        const pPrice = document.createElement("p");
+        pPrice.innerText = `${price}€/jour`;
+
         article.appendChild(img);
-        article.appendChild(h2);
+        article.appendChild(photographerName);
+        article.appendChild(localisation);
+        article.appendChild(pTagline);
+        article.appendChild(pPrice);
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, tagline, price, getUserCardDOM }
 }
